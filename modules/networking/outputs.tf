062139cf755678cc301bb5974c60035517e0e5e6
Zeerak Aziz <zeerak.aziz@madetech.com>
2025-04-04 14:36:38 +0100

    updating all scripts- deploying vpcs

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,9 +1,9 @@
 // Output the VPC ID so it can be reused elsewhere (e.g., in IAM, routing)
 output "vpc_id" {
-  value = aws_vpc.this.id
+  value = aws_vpc.sandbox_vpc.id
 }
 
 // Output the Subnet ID – this will be passed to the Databricks workspace module
 output "subnet_id" {
-  value = aws_subnet.this.id
+  value = aws_subnet.sandbox_subnet.id
 }
