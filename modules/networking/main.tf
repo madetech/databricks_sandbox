b84027fc89dc16159884a4d5f4bccff26da282ce
Zeerak Aziz <zeerak.aziz@madetech.com>
2025-04-04 14:36:38 +0100

    updating all scripts- deploying vpcs

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "sandbox_vpc" {
 }
 //Create a public subnet inside the vpc
 resource "aws_subnet" "sandbox_subnet" {
-  vpc_id                  = aws_vpc.sandbox_subnet.id
+  vpc_id                  = aws_vpc.sandbox_vpc.id
   cidr_block              = var.subnet_cidr
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
