4b9bf0fab3f92e8e57fff37e93f9dbccc3b6142e
Zeerak Aziz <zeerak.aziz@madetech.com>
2025-04-09 15:57:17 +0100

    sorted credentials login and fixing terraform apply errors

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -16,13 +16,20 @@ terraform {
       source  = "hashicorp/time"
       version = ">= 0.12.1"
     }
+  # }
+  #   backend "s3" {
+  #   bucket = "made-tech-databricks-sandbox-tfstate"
+  #   key    = "dev/terraform.tfstate"
+  #   region = "eu-west-2"
   }
 }
 
 #This block is required to authenticate and deploy AWS resources, and profile refers to AWS CLI profile
 provider "aws" {
   region  = var.region
-  profile = "databricks-sandbox"
+  skip_credentials_validation = false
+  skip_metadata_api_check     = false
+  skip_requesting_account_id  = false
 }
 
 provider "databricks" {
