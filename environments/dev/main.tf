e4621d4e903242e1dedfab8bc033f643e4aa4ac3
Zeerak Aziz <zeerak.aziz@madetech.com>
2025-04-11 17:21:26 +0100

    deployable Databricks sandbox with NAT fix and cluster bootstrap config

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -20,14 +20,15 @@ module "sra" {
   admin_user                = var.admin_user
   client_id                 = var.client_id
   client_secret             = var.client_secret
-  metastore_exists          = false  #tells module to create a new Unity Catalog metastore. this value needs to be manually switched later
+  metastore_exists          = true  #tells module to create a new Unity Catalog metastore. this value needs to be manually switched later
 
   network_configuration     = "isolated"
   vpc_cidr_range            = "10.0.0.0/16"
   private_subnets_cidr      = ["10.0.10.0/24", "10.0.11.0/24"]
-  privatelink_subnets_cidr  = ["10.0.3.0/24"]
+  public_subnets_cidr       = ["10.0.20.0/24", "10.0.21.0/24"]
+  privatelink_subnets_cidr  = ["10.0.30.0/24", "10.0.31.0/24"]
   availability_zones        = ["eu-west-2a","eu-west-2b"]
-  sg_egress_ports           = [443]
+  sg_egress_ports           = ["443", "2443", "6666", "8443", "8451"]
   region_bucket_name        = "sandbox-bucket-${var.region}"
-
+  log_delivery_mws_credentials_name = "Usage Delivery v2"
 }
