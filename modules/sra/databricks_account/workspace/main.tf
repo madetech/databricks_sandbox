52595bd7b0bd859634eea9582b17e2aad549c13c
Zeerak Aziz <zeerak.aziz@madetech.com>
2025-04-24 14:55:35 +0100

    Fix: renamed databricks credential to v2

--- modules/sra/databricks_account/workspace/main.tf
+++ modules/sra/databricks_account/workspace/main.tf
@@ -7,7 +7,7 @@ resource "time_sleep" "wait_30_seconds" {
 
 resource "databricks_mws_credentials" "this" {
   role_arn         = var.cross_account_role_arn
-  credentials_name = "${var.resource_prefix}-credentials"
+  credentials_name = "${var.resource_prefix}-credentials-v2"
   depends_on       = [time_sleep.wait_30_seconds]
 }
 
